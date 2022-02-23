7ad06433fac4cedc3894fdc8ba30fff909e73f1f
rattanmanna <rattanma@sheridancollege.ca>
2022-02-23 10:43:22 -0500

    edit done

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Manvir Singh Rattan
+ */
+public class TestUserprofile {
+    
+    public static void main (String []args){
+       
+        genre g1 = new genre();
+        System.out.println("Enter your name");
+        System.out.println("Choose you favourite genre ");
+        System.out.println(" Dance ,jazz, Rock");
+         System.out.println("Your Profile Has been Created");
+    }
+    
+}
